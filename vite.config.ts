ccc52e14e27df5c1ec78f90c259fd997dee99396
Bo [Mr Dong] <67427045+worldpeaceenginelabs@users.noreply.github.com>
2026-02-21 15:53:08 +0800

    SW skip waiting fix

--- vite.config.ts
+++ vite.config.ts
@@ -57,6 +57,8 @@ export default defineConfig({
         ],
       },
       workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         globIgnores: ['**/cesium/**'],
         maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
