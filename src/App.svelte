8a75e86b629d9070e9e4494c932a211cf1d4e272
Bo [Mr Dong] <67427045+worldpeaceenginelabs@users.noreply.github.com>
2026-08-06 08:57:23 +0800

    probability replaced by geometry

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,44 @@
 <script lang="ts">
+  import { onMount, onDestroy } from "svelte";
+
   import HexMenu from "./HexMenu.svelte";
   import Cesium from "./Cesium.svelte";
   import OverlayLayer from "./OverlayLayer.svelte";
 
   let tooltip = null;
+
+  let landscape = true;
+
+  function updateLayout() {
+    landscape = window.innerWidth >= window.innerHeight;
+  }
+
+  onMount(() => {
+    updateLayout();
+    window.addEventListener("resize", updateLayout);
+  });
+
+  onDestroy(() => {
+    window.removeEventListener("resize", updateLayout);
+  });
 </script>
 
 <div class="workspace">
 
   <div class="background-layer">
-    <HexMenu on:tooltip={(e) => tooltip = e.detail} />
+    <HexMenu on:tooltip={(e) => (tooltip = e.detail)} />
   </div>
 
-
-  <div class="globe-window">
-
+  <div
+    class="globe-window"
+    class:landscape={landscape}
+    class:portrait={!landscape}
+  >
     <div class="cesium-layer">
       <Cesium />
     </div>
-  
-    <OverlayLayer {tooltip}/>
-  
+
+    <OverlayLayer {tooltip} />
   </div>
 
 </div>
@@ -52,30 +70,26 @@
 
 .globe-window {
   position: absolute;
+  z-index: 20;
+}
 
+.globe-window.landscape {
   top: 0;
   right: 0;
-
   width: 50%;
   height: 100%;
+}
 
-  z-index: 20;
+.globe-window.portrait {
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 50%;
 }
 
 .cesium-layer {
   width: 100%;
   height: 100%;
-}
-
-@media (max-width: 768px) {
-  .globe-window {
-    top: auto;
-    bottom: 0;
-    left: 0;
-    right: 0;
-
-    width: 100%;
-    height: 50%;
-  }
 }
 </style>
