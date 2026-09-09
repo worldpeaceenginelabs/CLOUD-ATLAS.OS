2d78b3bc3afd74dbb90c2b5dfdc00e2118374500
Bo [Mr Dong] <67427045+worldpeaceenginelabs@users.noreply.github.com>
2026-09-09 18:29:42 +0800

    deleted oldstuff folder, copied useful to shared

--- src/AAA OLD STUFF/utils/keyboard.ts
+++
@@ -1,6 +0,0 @@
-export function onEnter(event: KeyboardEvent, handler: () => void): void {
-  if (event.key === 'Enter') {
-    handler();
-  }
-}
-
--- src/AAA OLD STUFF/utils/swarmMission.ts
+++
@@ -1,7 +0,0 @@
-import type { SwarmMissionLane, SwarmMissionState } from '../types';
-
-export function createEmptySwarmMissionState(): SwarmMissionState {
-  return {
-    links: { brainstorming: '', meetanddo: '', petition: '', crowdfunding: '' },
-  };
-}
